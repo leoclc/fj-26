6cd3a89ab4626ecec8afcb1491782485202994eb
Leonardo <leoclc@hotmail.com>
2013-01-21 21:59:33 -0200

    Exercicio 3.10

---
+++ src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
@@ -0,0 +1,21 @@
+package br.com.caelum.notasfiscais.mb;
+
+import javax.faces.bean.ManagedBean;
+
+import br.com.caelum.notasfiscais.dao.DAO;
+import br.com.caelum.notasfiscais.modelo.Produto;
+
+@ManagedBean
+public class ProdutoBean {
+	private Produto produto = new Produto();
+
+	public Produto getProduto() {
+		return this.produto;
+	}
+	
+	public void grava(){
+		DAO<Produto> dao = new DAO<Produto>(Produto.class);
+		dao.adiciona(produto);
+		this.produto = new Produto();
+	}
+}
