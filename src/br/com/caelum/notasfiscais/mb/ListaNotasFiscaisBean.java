64544b52e243376b765bd7910b6c0d8bfeb0adc7
Leonardo <leoclc@hotmail.com>
2013-01-25 19:58:27 -0200

    Exercicio 10.11

--- src/br/com/caelum/notasfiscais/mb/ListaNotasFiscaisBean.java
+++ src/br/com/caelum/notasfiscais/mb/ListaNotasFiscaisBean.java
@@ -1,28 +1,29 @@
 package br.com.caelum.notasfiscais.mb;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import org.primefaces.model.LazyDataModel;
+
 import br.com.caelum.notasfiscais.dao.DAO;
+import br.com.caelum.notasfiscais.datamodel.DataModelNotasFiscais;
 import br.com.caelum.notasfiscais.modelo.NotaFiscal;
 @ViewScoped
 @ManagedBean
 public class ListaNotasFiscaisBean {
-	
-	private List<NotaFiscal> notas = new ArrayList<NotaFiscal>();
 
-	public List<NotaFiscal> getNotas() {
+	private LazyDataModel<NotaFiscal> dataModel;
+
+	public ListaNotasFiscaisBean() {
+		this.dataModel = new DataModelNotasFiscais();
+		
 		DAO<NotaFiscal> dao = new DAO<NotaFiscal>(NotaFiscal.class);
-		this.notas = dao.listaTodos();
-		return notas;
+		this.dataModel.setRowCount(dao.contaTodos());
+	}
+	
+	public LazyDataModel<NotaFiscal> getDataModel() {
+	return this.dataModel;
 	}
 
-	public void setNotas(List<NotaFiscal> notas) {
-		this.notas = notas;
-	}
-	
-	
+		
 }
